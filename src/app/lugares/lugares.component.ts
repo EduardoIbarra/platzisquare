be00602b87908d2b438c8b73a2a7de07c6f8a508
Eduardo <ibredu@gmail.com>
2017-08-09 17:46:53 -0500

    Agregado animaciones a lista de destacados

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -8,23 +8,19 @@ import {trigger, state, style, transition, animate} from "@angular/animations";
     animations: [
         trigger('contenedorAnimable', [
             state('inicial', style({
-                opacity: 0,
-                backgroundColor: 'green',
-                transform: 'rotate3d(0,0,0,0deg)'
+                opacity: 0
             })),
             state('final', style({
-                opacity: 1,
-                backgroundColor: 'yellow',
-                transform: 'rotate3d(5,10,20,30deg)'
+                opacity: 1
             })),
-            transition('inicial => final', animate(1000)),
-            transition('final => inicial', animate(500)),
+            transition('inicial => final', animate(2000)),
+            transition('final => inicial', animate(1000)),
         ])
     ]
 })
 export class LugaresComponent {
     title = 'PlatziSquare';
-    state = 'final';
+    state = 'inicial';
 
     lat:number = 4.6560663;
     lng:number = -74.0595918;
@@ -46,6 +42,7 @@ export class LugaresComponent {
                 this.lugares = lugares;
                 var me = this;
                 me.lugares = Object.keys(me.lugares).map(function (key) { return me.lugares[key]; });
+                this.state = 'final';
             }, error => {
                 console.log(error);
                 alert('Tenemos algo de dificultades, disculpe las molestias. Error: ' + error.statusText);
