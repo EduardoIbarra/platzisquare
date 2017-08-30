71d1be97aaf22c9d3287527f0b6121b60ce6e19f
Eduardo <ibredu@gmail.com>
2017-08-30 18:50:43 -0500

    bootstrap implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,11 @@ import { AgmCoreModule } from '@agm/core';
 import {ResaltarDirective} from "./directives/resaltar.directive";
 import {ContarClicksDirective} from "./directives/contar-clicks.directive";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {Routes, RouterModule} from "@angular/router";
-import {DetalleComponent} from "./detalle/detalle.component";
-import {LugaresComponent} from "./lugares/lugares.component";
-const appRoutes: Routes = [
-  {path:'', component: LugaresComponent},
-  {path:'lugares', component: LugaresComponent},
-  {path:'detalle', component: DetalleComponent},
-];
 @NgModule({
   declarations: [
     AppComponent,
       ResaltarDirective,
-      ContarClicksDirective,
-      DetalleComponent,
-      LugaresComponent
+      ContarClicksDirective
   ],
   imports: [
     BrowserModule,
@@ -29,8 +19,7 @@ const appRoutes: Routes = [
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCiGsoFevMN2J-dXWtD_31AN4UkraR4Hq0'
     }),
-    BrowserAnimationsModule,
-      RouterModule.forRoot(appRoutes)
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
